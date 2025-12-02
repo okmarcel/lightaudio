c481906e509f520e5e1e52760f677b20d2e1ef96
okmarcel <marcelgladysz44@gmail.com>
2025-12-02 01:00:38 +0100

    change structure to header-only

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # lightaudio
 Project goal: a lightweight audio and DSP library in C, suited for embedded programming.
+Status: work in progress
+
+## Build
+run
+```
+mkdir build
+cd build
+cmake ..
+make install
+```
+
+## Using in project
+In CMake add
+```
+find_package(lightaudio REQUIRED)
+target_link_libraries(myapp PRIVATE lightaudio::lightaudio)
+```
+and in program file add
+```
+#include <lightaudio/lightaudio.h>
+```
 
--- include/lightaudio/buffer.h
+++ include/lightaudio/buffer.h
@@ -11,18 +11,19 @@ typedef struct {
 	float *data;			// Memory provided by user
 	uint32_t capacity;		// Total number of samples
 	uint32_t write_index;	// Circular write pointer
+	uint32_t size;
 } la_buffer;
 
 /* Initialize buffer
  * 'memory' is an array declared by user
  */
 static inline void la_buffer_init(la_buffer *buffer, const float *memory, const uint32_t capacity) {
-	buffer->data = memory;
-	buffer->capacity = capacity;
+	buffer->data = (float *)memory;
+	buffer->capacity = (uint32_t)capacity;
 	buffer->write_index = 0;
 
 	// Fill buffer with zeros for safety
-	for (uint32_t i = 0; i < size; i++) {
+	for (uint32_t i = 0; i < buffer->size; i++) {
 		buffer->data[i] = 0.0f;
 	}
 
