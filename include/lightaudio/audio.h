f21ca403b1fd1f625b0ffbc35c7adc4693cd3020
okmarcel <marcelgladysz44@gmail.com>
2025-12-02 21:35:56 +0100

    add audio conifg

---
+++ include/lightaudio/audio.h
@@ -0,0 +1,26 @@
+#ifndef LA_AUDIO_H
+#define LA_AUDIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	uint32_t sample_rate;
+	uint32_t block_size;
+} la_audio_config;
+
+/* CONFIG FUNCTIONS */
+static inline void la_audio_config_make(uint32_t sample_rate) {
+	la_audio_config config;
+	config.sample_rate = sample_rate;
+	config.block_size = block_size;
+	return config;
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#endif
+
